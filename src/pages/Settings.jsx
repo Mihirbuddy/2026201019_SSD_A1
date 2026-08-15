68f9cbf45b69655857e0f9cd8465e04ac652c5f9
mihirbuddy <pradhanmihir17@gmail.com>
2026-08-15 20:28:57 +0530

    add name validation and update logic

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -3,11 +3,21 @@ import { useState } from "react";
 function Settings({ currentUser, onNameChange }) {
 
   const [name, setName] = useState(currentUser.name);
+  function handleSubmit(event) {
+  event.preventDefault();
+
+  if (name.trim() === "") {
+    alert("Name cannot be empty.");
+    return;
+  }
+
+  onNameChange(name.trim());
+}
   return (
     <div className="page">
       <h1>Settings</h1>
 
-      <form className="settings-form">
+     <form className="settings-form" onSubmit={handleSubmit}>
         <label>Change your name</label>
 
         <input
