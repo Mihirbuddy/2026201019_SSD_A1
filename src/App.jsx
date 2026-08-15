e92c659e1de7a3c2102ac76a88fc00832a49e364
mihirbuddy <pradhanmihir17@gmail.com>
2026-08-15 20:38:40 +0530

    add logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
@@ -9,11 +10,24 @@ function App() {
     setCurrentUser(user);
   }
 
+  function handleLogout() {
+    setCurrentUser(null);
+  }
+
   if (!currentUser) {
     return <Login onLogin={handleLogin} />;
   }
 
-  return <h1>Welcome to the application</h1>;
+  return (
+    <>
+      <Navbar
+        currentUser={currentUser}
+        onLogout={handleLogout}
+      />
+
+      <h1>Welcome to the application</h1>
+    </>
+  );
 }
 
 export default App;
