6e44d5c76ede3bb98394b177d9d1902b6cb67ec1
mihirbuddy <pradhanmihir17@gmail.com>
2026-08-15 16:30:58 +0530

    created dummy users data

---
+++ src/data/users.js
@@ -0,0 +1,32 @@
+export const users = [
+  {
+    id: 1,
+    name: "Rahul Sharma",
+    role: "student",
+    email: "rahul@example.com",
+  },
+  {
+    id: 2,
+    name: "Priya Verma",
+    role: "student",
+    email: "priya@example.com",
+  },
+  {
+    id: 3,
+    name: "Aman Singh",
+    role: "student",
+    email: "aman@example.com",
+  },
+  {
+    id: 4,
+    name: "Dr. Mehta",
+    role: "teacher",
+    email: "mehta@example.com",
+  },
+  {
+    id: 5,
+    name: "Dr. Sharma",
+    role: "teacher",
+    email: "sharma@example.com",
+  },
+];
